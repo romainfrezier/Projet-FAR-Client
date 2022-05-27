b74b023be2a332fdd311704f6e796e3fd679dccb
Romain FREZIER <romain.frezier@etu.umontpellier.fr>
2022-05-27 15:35:24 +0200

    ip in param

--- scriptClient.sh
+++ scriptClient.sh
@@ -1,2 +1,2 @@
 gcc -o client main.c lib/src/client.c lib/src/colors.c lib/src/tools.c lib/src/stringFunc.c lib/src/fileClient.c lib/src/commandClient.c lib/src/list.c -lpthread;
-./client 127.0.0.1 $1;
+./client $1 $2;
